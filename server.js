bce192c604e6837aef797316bfb7b451b0b23a98
on4r <on4r@protonmail.com>
2020-09-17 15:26:27 -0300

    suba. an rouder etz

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ const { validPlaylist } = require("./parser")
  * Settings and Configuration
  */
 const APP = express()
+const ROUTER = express.Router()
 const PORT = process.env["YPT_APP_PORT"] || 8080
+const BASE_URL = process.env["YPT_APP_BASE_URL"] || "/"
 const messages = function(data) {
 	return {
 		invalid: `Please enter a <em>valid</em> and <em>public</em> playlist ID.`,
-		info: `We already indexed this playlist. You can find it <a href='/${data.id}'>here</a>.`,
-		success: `Alright, we will periodically check your playlist for deleted videos now!<br>You can check the current status <a href="/${data.id}">here</a>.`,
+		info: `We already indexed this playlist. You can find it <a href='${BASE_URL}${data.id}'>here</a>.`,
+		success: `Alright, we will periodically check your playlist for deleted videos now!<br>You can check the current status <a href="${BASE_URL}${data.id}">here</a>.`,
 		dberror: `Ups! Something went wrong. Please try again later.`,
 		dbupdate: `We are currently updating our database. Please try again later.`
 	}
@@ -27,33 +29,17 @@ const restoredVideosFirst = function(a, b) {
 APP.set("view engine", "ejs")
 
 /*
- * Middleware
- */
-
-APP.use(bodyParser.urlencoded({ extended: true }))
-APP.use(express.static(__dirname + '/public'))
-APP.use(function(req, res, next) {
-	if (CRON.updateInProgress()) {
-		res.sendStatus(503)
-		return
-	} else {
-		next()
-	}
-})
-
-/*
  * Routes
  *
  * Home: GET /
  * Form: POST /
  * Playlist: GET /*
  */
-
-APP.get("/", function(req, res) {
+ROUTER.get("/", function(req, res) {
 	res.render("pages/home", { message: null, type: null })
 })
 
-APP.post("/", async function(req, res) {
+ROUTER.post("/", async function(req, res) {
 
 	const id = req.body.playlist_id
 	let playlist = null
@@ -98,9 +84,9 @@ APP.post("/", async function(req, res) {
 
 })
 
-APP.get("/*", async function(req, res) {
+ROUTER.get("/*", async function(req, res) {
 
-	let id = req.originalUrl.substring(1)
+	let id = req.url.substring(1)
 	let playlist = null
 	let videoIds = []
 	let deletedVideos = []
@@ -163,10 +149,38 @@ APP.get("/*", async function(req, res) {
 
 })
 
+/*
+ * Middleware
+ */
+
+APP.use(bodyParser.urlencoded({ extended: true }))
+APP.use(express.static(__dirname + '/public'))
+
+console.log(__dirname)
+// temporarily unavailable when updating database
+APP.use(function(req, res, next) {
+	if (CRON.updateInProgress()) {
+		res.sendStatus(503)
+		return
+	} else {
+		next()
+	}
+})
+
+// pass base_url to templates
+APP.use(function(req, res, next) {
+	res.locals.base_url = BASE_URL
+	next()
+})
+
+// set a base url for the app
+// so we can host it in a subdir
+APP.use(BASE_URL, ROUTER)
 
 
 APP.listen(PORT)
 console.log("APP: listening at port", PORT)
+console.log("APP: base url", BASE_URL)
 
 CRON.startDailyUpdate()
 
