14ffcf8055fa6b2352a3042fb1a51746bdd5839c
on4r <on4r@protonmail.com>
2020-10-02 18:32:44 -0300

    tabindex for the win

--- modules/router.js
+++ modules/router.js
@@ -249,8 +249,8 @@ async function getDeletedVideos(req, res, next) {
 function messages({id} = {}) {
 	return {
 		invalid: `Please enter a <em>valid</em> and <em>public</em> playlist ID.`,
-		info: `We already indexed this playlist. You can find it <a href='./${id}'>here</a>.`,
-		success: `Alright, we will periodically check your playlist for deleted videos now!<br>You can check the current status <a href="./${id}">here</a>.`,
+		info: `We already indexed this playlist. You can find it <a tabindex="1" href='./${id}'>here</a>.`,
+		success: `Alright, we will periodically check your playlist for deleted videos now!<br>You can check the current status <a tabindex="1" href="./${id}">here</a>.`,
 		dberror: `Ups! Something went wrong. Please try again later.`,
 		dbupdate: `We are currently updating our database. Please try again later.`
 	}
